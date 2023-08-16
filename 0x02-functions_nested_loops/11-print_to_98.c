c6463dc879292bb39a4257c012f19dc7a9f700ac
Toqa <toqa.ashraf235@gmail.com>
2023-08-15 19:02:45 -0700

    12

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,36 +1,28 @@
+/*
+ * File: 11-print_to_98.c
+ * Aut: toqa
+ */
+
 #include <stdio.h>
-#include "6-abs.c"
-#include "main.h"
 
 /**
- * print_times_table - whatever man
- *@n: integer to put in this table
- *
+ * print_to_98 - Prints all natural numbers from input to 98,
+ *               in order separated by a comma followed by a space.
+ * @n: The number to begin counting at.
  */
 void print_to_98(int n)
 {
-	int i;
-
-	if (n > 98)
+	if (n >= 98)
 	{
-		for (i = n; i >= 98; i--)
-		{
-			printf("%d", i);
-			if (i != 98)
-			{
-				printf(", ");
-			}
-		}
-	} else
-	{
-		for (i = n; i <= 98; i++)
-		{
-			printf("%d", i);
-			if (i != 98)
-			{
-				printf(", ");
-			}
-		}
+		while (n > 98)
+			printf("%d, ", n--);
+		printf("%d\n", n);
 	}
-	printf("\n");
+
+	else
+	{
+		while (n < 98)
+			printf("%d, ", n++);
+		printf("%d\n", n);
+	}
 }
