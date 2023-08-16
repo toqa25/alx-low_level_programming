2c09466aa29b8f180cf8612085aa30e0bec036d9
Toqa <toqa.ashraf235@gmail.com>
2023-08-15 18:13:15 -0700

    11

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * add - adding it up
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: sum of both integers
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
