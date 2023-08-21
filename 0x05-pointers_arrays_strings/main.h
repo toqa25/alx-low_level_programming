9e295efd470748d339eef47e41bd41b5ca71ad7d
Toqa <toqa.ashraf235@gmail.com>
2023-08-21 16:50:03 -0700

    6

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -3,21 +3,21 @@
 #include <stdio.h>
 
 /**
- * void prototypes
- * char prototypes
- * int prototypes
- * */
-
+* void prototypes
+*char prototypes
+* int prototypes
+*/
 void reset_to_98(int *n);
-void swap_int(int *a, int *b);
-int _strlen(char *s);
 void _puts(char *str);
-int _putchar(char c);
 void print_rev(char *s);
+void rev_string(char *s);
+void swap_int(int *a, int *b);
+void swap_char(char *a, char *b);
 void puts2(char *str);
 void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
-void rev_string(char *s);
-
-#endif /* MAIN_H */
+int _putchar(char c);
+int _strlen(char *s);
+int _atoi(char *s);
+#endif /*MAIN_H*/
