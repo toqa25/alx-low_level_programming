872477422283a061c2b1e9d0ac2608206e2abe02
Toqa <toqa.ashraf235@gmail.com>
2023-08-21 16:16:10 -0700

    0

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,18 +1,12 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
- * main-check the code 
- *
- * Return: Always 0.
+ *reset value of an integer to 98
+ *@n : targeted parameter
+ *Return : nothing
  */
-int main(void)
-{
-    int n;
 
-    n = 402;
-    printf("n=%d\n", n);
-    reset_to_98(&n);
-    printf("n=%d\n", n);
-    return (0);
+void reset_to_98(int *n)
+{
+	*n = 98;
 }
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,5 +1,12 @@
 #ifndef MAIN_H
 #define MAIN_H
+#include <stdio.h>
+
+/**
+ * void prototypes
+ * char prototypes
+ * int prototypes
+ * */
 
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
