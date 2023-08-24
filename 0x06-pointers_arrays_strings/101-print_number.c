4864e871f86f91caffa18ca02a9055aa801b9664
Toqa <toqa.ashraf235@gmail.com>
2023-08-24 14:41:56 -0700

    9

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -9,18 +9,11 @@
  */
 void print_number(int n)
 {
-	unsigned int num;
-/*check if number is negative*/
-	num = n;
-	if (n < 0)
-	{
-		_putchar(45);
-		num = -n;
-	}
-/* print number by recursion*/
-	if (num / 10)
-	{
-		print_number(num / 10);
-	}
-	_putchar((num % 10) + '0');
+    if (n < 0) {
+        _putchar('-');
+        n = -n;
+    }
+    if (n / 10)
+        print_number(n / 10);
+    _putchar(n % 10 + '0');
 }
