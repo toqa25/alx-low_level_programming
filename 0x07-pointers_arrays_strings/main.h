f793814251f7bebef23048b6ce18bf346c467c74
Toqa <toqa.ashraf235@gmail.com>
2023-08-28 15:08:11 -0700

    0

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -3,6 +3,9 @@
 /**
  *pointers
  */
+#include <stdio.h>
+#include <stddef.h>
+
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
@@ -12,5 +15,5 @@ char *_strstr(char *haystack, char *needle);
 void print_chessboard(char (*a)[8]);
 void print_diagsums(int *a, int size);
 
-#endif
+#endif /*MAIN_H*/
 
